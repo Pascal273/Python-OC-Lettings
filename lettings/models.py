df4f111ff61dee6bf696c5717bbfe741225ac323
Pascal <77117642+Pascal273@users.noreply.github.com>
2022-09-15 21:13:35 +0200

    Created new app lettings, containing the address and letting models

--- lettings/models.py
+++ lettings/models.py
@@ -1,6 +1,5 @@
 from django.core.validators import MaxValueValidator, MinLengthValidator
 from django.db import models
-from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 
@@ -25,11 +24,3 @@ class Letting(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    favorite_city = models.CharField(max_length=64, blank=True)
-
-    def __str__(self):
-        return self.user.username
